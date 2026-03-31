1f5830b9773abcd86609383196da7fd9f955ab53
Lina Sandberg Müller <lisa5634@p52.uppmax.uu.se>
2026-03-31 12:43:58 +0200

    Initial project structure, scripts, and .gitignore setup

---
+++ code/1_fastqc_WG_raw.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ladda moduler
+module load FastQC
+
+# FastQC on whole genome raw data 
+fastqc -t 2 data/raw_data/whole_genome_data/*.fq.gz -o analyses/01_preprocessing/fastqc_raw/
