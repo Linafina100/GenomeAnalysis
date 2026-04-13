303286b89e18403e32d695e6637897bfa848bb54
Lina Sandberg Müller <lisa5634@pelle2.uppmax.uu.se>
2026-04-13 21:18:57 +0200

    fixed time

--- code/4_flye_assembly_chr3.sh
+++ code/4_flye_assembly_chr3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
-#SBATCH -c 2
+#SBATCH -c 16
 #SBATCH --mem=64G
-#SBATCH -t 12:00:00
+#SBATCH -t 5:00:00
 #SBATCH -J flye_chr3
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lina.sandberg-muller.5634@student.uu.se
-#SBATCH --output=%x.%j.out
+#SBATCH --output=logs/%x.%j.out
 
 # Load the necessary modules
 module load Flye
@@ -15,4 +15,4 @@ module load Flye
 # Run Flye assembly using your specific folder paths
 flye --nano-raw data/raw_data/chr3_clean_nanopore.fq.gz \
      --out-dir analyses/02_genome_assembly/flye_output \
-     --threads 2
+     --threads 16
