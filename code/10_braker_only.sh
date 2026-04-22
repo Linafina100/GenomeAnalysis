3166cae62653c781f1814b364140d7926761a01f
Lina Sandberg Müller <lisa5634@pelle1.uppmax.uu.se>
2026-04-22 13:18:37 +0200

    Script for braker (non-functional

--- code/10_braker_only.sh
+++ code/10_braker_only.sh
@@ -1,39 +1,35 @@
 #!/bin/bash -l
-
-# slurm directives
 #SBATCH -A uppmax2026-1-61
 #SBATCH -p pelle
-#SBATCH -c 2                           
-#SBATCH -t 30:00:00
-#SBATCH -J braker3_only
+#SBATCH -c 2
+#SBATCH -t 24:00:00
+#SBATCH --mem=64G
+#SBATCH -J braker_only
+#SBATCH --mail-type=ALL
 #SBATCH --output=/home/lisa5634/GenomeAnalysis/analyses/05_annotation/02_structure_annotation/%x.%j.out
 
-# module set up
-module load braker/3.0.3
-
-# directories & files
+# file variables
 OUT_DIR="/home/lisa5634/GenomeAnalysis/analyses/05_annotation/02_structure_annotation"
-GENOME="/home/lisa5634/GenomeAnalysis/analyses/05_annotation/01_repeat_masking/chr3_polished.fasta.masked"
-BAM_FILE="${OUT_DIR}/mapped_RNA_Aligned.sortedByCoord.out.bam"
-PROT_DB="/home/lisa5634/GenomeAnalysis/data/reference_data/C_purpureus.faa"
-SIF_FILE="/crex/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/braker3.sif"
+MASKED_GENOME="/home/lisa5634/GenomeAnalysis/analyses/05_annotation/01_repeat_masking/chr3_polished.fasta.masked"
+BRAKER_SIF="/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/braker3.sif"
+BAM_FILE="$OUT_DIR/mapped_RNA_Aligned.sortedByCoord.out.bam"
 
-# structural annotation
-echo "Starting BRAKER3 annotation..."
+# setup output directory
+mkdir -p $OUT_DIR/braker_output
+export TMPDIR=$OUT_DIR/tmp
+mkdir -p $TMPDIR
 
-export SINGULARITYENV_AUGUSTUS_CONFIG_PATH=/home/lisa5634/bin/augustus_config
+
+# run braker3 container
+#export AUGUSTUS_CONFIG_PATH="/home/lisa5634/bin/augustus_config"
 
 singularity exec \
     -B /home/lisa5634:/home/lisa5634 \
-    ${SIF_FILE} \
-    braker.pl --genome=${GENOME} \
-              --bam=${BAM_FILE} \
-              --prot_seq=${PROT_DB} \
+    -B /home/lisa5634/bin/augustus_config:/opt/Augustus/config \
+    $BRAKER_SIF \
+    braker.pl --genome=$MASKED_GENOME \
+              --bam=$BAM_FILE \
               --softmasking \
-              --species=niphotrichum_japonicum_lina \
-              --AUGUSTUS_CONFIG_PATH=/home/lisa5634/bin/augustus_config \
+              --species=lisa2 \
               --threads=2 \
-              --workingdir=${OUT_DIR}/braker_out
-
-echo "BRAKER3 annotation completed successfully."
-
+              --workingdir=$OUT_DIR/braker_output
