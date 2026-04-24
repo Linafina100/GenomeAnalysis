eae4c648132e4ccdf5021ac74416eb72af4e0f6e
Lina Sandberg Müller <lisa5634@pelle1.uppmax.uu.se>
2026-04-24 08:15:40 +0200

    Fixed to only star

---
+++ code/9_star_mapping.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-61
+#SBATCH -p pelle
+#SBATCH -c 8
+#SBATCH -t 40:00:00
+#SBATCH -J star_braker
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/lisa5634/GenomeAnalysis/analyses/05_structural_annotation/02_structure_annotation/%x.%j.out
+
+# load modules
+module load STAR/2.7.11b-GCC-13.3.0
+module load SAMtools/1.22.1-GCC-13.3.0
+
+# file variable
+MASKED_GENOME="/home/lisa5634/GenomeAnalysis/analyses/05_structural_annotation/01_repeat_masking/chr3_polished.fasta.masked"
+
+# rna files
+RNA_DIR="/proj/uppmax2026-1-61/Genome_Analysis/2_Zhou_2023/reads/transcriptomic_data"
+RNA_R1="${RNA_DIR}/Control_1_f1.fq.gz,${RNA_DIR}/Control_2_f1.fq.gz,${RNA_DIR}/Control_3_f1.fq.gz,${RNA_DIR}/Heat_treated_42_12h_1_f1.fq.gz,${RNA_DIR}/Heat_treated_42_12h_2_f1.fq.gz,${RNA_DIR}/Heat_treated_42_12h_3_f1.fq.gz"
+RNA_R2="${RNA_DIR}/Control_1_r2.fq.gz,${RNA_DIR}/Control_2_r2.fq.gz,${RNA_DIR}/Control_3_r2.fq.gz,${RNA_DIR}/Heat_treated_42_12h_1_r2.fq.gz,${RNA_DIR}/Heat_treated_42_12h_2_r2.fq.gz,${RNA_DIR}/Heat_treated_42_12h_3_r2.fq.gz"
+
+# setup output directory
+OUT_DIR="/home/lisa5634/GenomeAnalysis/analyses/05_annotation/02_structure_annotation"
+mkdir -p $OUT_DIR/star_index
+
+# build star index
+STAR --runThreadN 8 \
+     --runMode genomeGenerate \
+     --genomeDir $OUT_DIR/star_index \
+     --genomeFastaFiles $MASKED_GENOME
+
+# map rna reads
+STAR --runThreadN 8 \
+     --genomeDir $OUT_DIR/star_index \
+     --readFilesIn $RNA_R1 $RNA_R2 \
+     --readFilesCommand zcat \
+     --outSAMtype BAM SortedByCoordinate \
+     --outFileNamePrefix $OUT_DIR/mapped_RNA_
+
+
